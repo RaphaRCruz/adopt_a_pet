6e626cacd8f6b2ef053b806eab8af5594805114a
Raphael Cruz <raphacruz_90@hotmail.com>
2022-09-27 11:01:24 -0300

    routes and seeds

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,53 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = Pet.where(adopted: false)
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to root_path, notice: 'Add sucess!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to root_path, notice: 'Update sucess!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to root_path
+  end
+
+  def adoption
+    @pets = Pet.where(adopted: true)
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :category, :birth, :adopted, :owner, :adoption_time)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Pet.destroy_all
+
+puts 'Creating pets...'
+rock = { name: 'Rock', category: 'Dog', birth: Date.new(2019, 06, 13), adopted: false, owner: nil, adoption_time: nil }
+apollo = { name: 'Apollo', category: 'Dog', birth: Date.new(2021, 03, 03), adopted: false, owner: nil, adoption_time: nil }
+atila = { name: 'Átila', category: 'Dog', birth: Date.new(2021, 11, 02), adopted: false, owner: nil, adoption_time: nil }
+simba = { name: 'Simba', category: 'Cat', birth: Date.new(2020, 01, 25), adopted: false, owner: nil, adoption_time: nil }
+hunter = { name: 'Hunter', category: 'Cat', birth: Date.new(2020, 12, 07), adopted: false, owner: nil, adoption_time: nil }
+
+[rock, apollo, atila, simba, hunter].each do |attributes|
+  pet = Pet.create!(attributes)
+  puts "Created #{pet.name}"
+end
+puts 'Finished!'
